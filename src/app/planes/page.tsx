5f303a895178a59538c47583779421e630da3cec
luishdz04 <ing.luisdeluna@outlook.com>
2025-06-20 21:39:30 -0600

    Update page.tsx

--- src/app/planes/page.tsx
+++ src/app/planes/page.tsx
@@ -132,7 +132,7 @@ export default function PlanesPage() {
   const router = useRouter();
 
   const handleInscribirse = () => {
-    router.push('/registromup');
+    router.push('/registro/paso1');
   };
 
   // Animaciones para el contenedor
