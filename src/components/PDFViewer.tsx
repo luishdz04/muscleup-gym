649d0512cea04a603f94af8ed29d8d4f8b1ee10b
luishdz04 <ing.luisdeluna@outlook.com>
2025-09-22 12:55:16 -0600

    feat: Implement PDF viewer system with react-pdf

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -6,7 +6,7 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.mjs`;
 
 
 
